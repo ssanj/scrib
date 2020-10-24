cc831e57267a0b6c569967818be6bab902f7f850
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-10-24 20:17:50 +1100

    get notes saved to session storage

--- ports.js
+++ ports.js
@@ -12,7 +12,48 @@ function portActions(action) {
   // } else if (noteEvent.eventType=== "log_message_to_console") {
   if (action.eventType === "log_action") {
     console.log("log: " + JSON.stringify(action.data));
+  } else if (action.eventType === "storage_action") {
+    handleStorage(action)
   } else {
     console.log("error, handled action:" + JSON.stringify(action));
   }
 }
+
+//we need a way to figure out how to respond to JS
+function handleStorage(action) {
+  const storage       = action.storage;
+  const store         = findStorageType(storage.storageType)
+  const storageAction = storage.storageAction;
+
+  if (storageAction === "save") {
+    store.setItem(storage.storageKey, JSON.stringify(storage.data));
+    console.log("storage saved");
+  } else if (storageAction === "delete") {
+    store.removetem(storage.storageKey);
+    console.log("storage deleted");
+  } else if (storageAction === "load") {
+    store.getItem(storage.storageKey); //fix
+    console.log("storage loaded");
+  } else if (storageAction === "clear") {
+    store.clear();
+    console.log("storage cleared");
+  } else {
+    console.log("unknown storage action: " + JSON.stringify(action.storage));
+  }
+}
+
+// function subscriptionAction(eventType, result) {
+//   {
+//     "eventType": eventType,
+//     "data": JSON.parse(result);
+//   }
+// }
+
+function findStorageType(storageType) {
+  if (storageType === "session") {
+    return sessionStorage;
+  } else {
+    return localStorage
+  }
+}
+
