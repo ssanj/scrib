bbcac1dc71ad2b0e8f6e485794c087c02886461b
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-10-31 16:22:17 +1100

    add findMap

--- src/FP.elm
+++ src/FP.elm
@@ -23,9 +23,26 @@ collect predicate elements =
 maybeToList : Maybe a -> List a
 maybeToList = maybe [] List.singleton
 
-
+-- Returns first match
 find : (a -> Bool) -> List a -> Maybe a
 find predicate elements =
   case elements of
     [] -> Nothing
     (x::xs) -> if predicate x then Just x else find predicate xs
+
+
+{-| Returns first instance where a match is found and exists early.
+
+    Finds the first instance where `a` can be converted to a `Just b`
+    or a returns Nothing if none of the values can be converted.
+
+    The benefit of `findMap` over `collect` is that, `findMap` will
+    exit after it finds a single match, where as `collect` will still
+    navigate the whole list looking for *all* matches.
+-}
+findMap : (a -> Maybe b) -> List a -> Maybe b
+findMap f elements =
+  case elements of
+    [] -> Nothing
+    (x::xs) -> maybe (findMap f xs) Just <| f x
+
