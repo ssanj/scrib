b55df01c0ad9eb5972a409c406d788647ad398bd
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-01-05 17:04:29 +1100

    update build-optimized

--- build-optimized.sh
+++ build-optimized.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-elm make src/${1}.elm --optimize --output="${2}.js"
+if [ "$#" -ne 2 ]; then
+    echo "Please supply <name of Elm file> <name of js file>"
+    echo "extensions should be omitted in either case"
+    echo "eg. build-optimized.sh Save scrib-save"
+    exit 1
+fi
+
+elm make src/${1}.elm --optimize --output="resources/js/${2}.js"
