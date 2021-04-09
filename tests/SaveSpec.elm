497b7919eee972f838d1b51d57ceff938d778179
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-04-09 10:07:36 +1000

    Add more tests around busy states

--- tests/SaveSpec.elm
+++ tests/SaveSpec.elm
@@ -1,28 +1,50 @@
 module SaveSpec exposing (..)
 
-import Test exposing (..)
+import Test             exposing (..)
+
 
 import Expect      exposing (Expectation)
 import FP          exposing (const)
 import Save        exposing (isBusy, WhatAreWeDoing(..), viewNewNoteButton)
+
 import Test.Html.Selector as Selector
+import List.Nonempty      as NE
 
 import Test.Html.Query as Query
+
+
+busyStates : NE.Nonempty WhatAreWeDoing
+busyStates =
+  NE.Nonempty SavingNoteRemotely
+    [
+      UpdatingSessionCache
+    , DeletingNoteRemotely
+    , DeletingNoteFromSessionCache
+    ]
+
+
+whatAreWeDoingString : WhatAreWeDoing -> String
+whatAreWeDoingString doing =
+  case doing of
+    SavingNoteRemotely            -> "SavingNoteRemotely"
+    SavingNoteLocally             -> "SavingNoteLocally"
+    UpdatingSessionCache          -> "UpdatingSessionCache"
+    DeletingNoteRemotely          -> "DeletingNoteRemotely"
+    DeletingNoteFromSessionCache  -> "DeletingNoteFromSessionCache"
+    Idle                          -> "Idle"
 
 
 isBusyWhenBusy : Test
 isBusyWhenBusy =
   test "Returns True when busy" <|
     \_ ->
-      let actualList   = List.map isBusy
-                          [
-                            SavingNoteRemotely
-                          , UpdatingSessionCache
-                          , DeletingNoteRemotely
-                          , DeletingNoteFromSessionCache
-                          ]
-          expected = True
-      in Expect.equal (List.all (const True) actualList) expected
+      let expected = True
+          expectToBeBusy : WhatAreWeDoing -> Expectation
+          expectToBeBusy doing = Expect.true ("is busy shouldn't have been True for " ++ (whatAreWeDoingString doing)) <| isBusy doing
+
+          allExpectations = NE.map expectToBeBusy busyStates
+      in combineExpectations allExpectations
+
 
 isBusyWhenIdle : Test
 isBusyWhenIdle =
@@ -31,6 +53,7 @@ isBusyWhenIdle =
       let actual   = isBusy Idle
           expected = False
       in Expect.equal actual expected
+
 
 viewNewNoteButtonWhenIdle : Test
 viewNewNoteButtonWhenIdle =
@@ -45,5 +68,16 @@ viewNewNoteButtonWhenBusy : Test
 viewNewNoteButtonWhenBusy =
   test "Should be disabled when busy" <|
     \_ ->
-      let html = viewNewNoteButton SavingNoteRemotely
-      in Query.has [Selector.class "is-static"] <| Query.fromHtml html
+      let expectViewNoteButtonIsDisabled :  WhatAreWeDoing -> Expectation
+          expectViewNoteButtonIsDisabled doing =
+            let html = viewNewNoteButton doing
+            in Query.has [Selector.class "is-static"] <| Query.fromHtml html
+          allExpectations = NE.map expectViewNoteButtonIsDisabled busyStates
+      in combineExpectations allExpectations
+
+
+combineExpectations : NE.Nonempty Expectation -> Expectation
+combineExpectations nel =
+  let expectations : NE.Nonempty (a -> Expectation)
+      expectations = NE.map const nel
+  in  Expect.all (NE.toList expectations) ()
