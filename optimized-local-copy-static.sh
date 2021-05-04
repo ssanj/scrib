238bc59d0a6833b3f1744ecc4432cce3dab3b496
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-05-05 09:45:52 +1000

    Add page compilation to build

--- optimized-local-copy-static.sh
+++ optimized-local-copy-static.sh
@@ -6,5 +6,7 @@ echo "Compiling Save"
 makeSave
 echo "Compiling Config"
 makeConfig
+echo "Compiling Page"
+makePage
 
 optimized-copy-static.sh '/tmp/minify-staging' '/Volumes/Work/projects/code/haskell/toy/slate/static/'
